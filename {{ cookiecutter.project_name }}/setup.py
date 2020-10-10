528ce5510ca2345298dc5ddc8c2bdb5961eb8117
Anouar Zbaida <anouar6in@gmail.com>
2020-10-10 17:50:08 +0200

    modify: name the project folder with the slug

--- {{ cookiecutter.project_name }}/setup.py
+++ {{ cookiecutter.project_name }}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='{{cookiecutter.project_name}}',
+    name='{{cookiecutter.project_slug}}',
     packages=find_packages(),
     version='{{cookiecutter.version}}',
     description='{{cookiecutter.description}}',
